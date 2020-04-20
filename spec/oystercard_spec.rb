f582998f88a1c6858569a82c2fbd52b2a1306b41
patrick <patrick_sawyer@hotmail.co.uk>
2020-04-20 16:50:56 +0100

    removes unnecessary responds_to(:balance) test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,12 +3,11 @@ require 'oystercard'
 describe Oystercard do
 
   describe 'balance' do
-    it 'responds to balance' do
-      expect(subject).to respond_to(:balance)
-    end
   
     it 'balance has 0' do
       expect(subject.balance).to eq(0)
     end
+
   end
+  
 end
